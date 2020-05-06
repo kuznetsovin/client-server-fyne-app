3f31dc73c26d55f6ebd167f9b42aeee1ee1c1362
kuznetsovin <kuznetsovin@gmail.com>
2020-05-06 11:28:35 +0300

    add log and config file

--- client/main.go
+++ client/main.go
@@ -6,9 +6,29 @@ import (
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
+	//create log file
+	logFile, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
+	// get server add from file
+	srv, err := ioutil.ReadFile("server.ini")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srvAddr := string(srv)
+	log.Println("Server addr: ", srvAddr)
+
 	a := app.New()
 
 	//create new window
@@ -23,6 +43,12 @@ func main() {
 
 	btnSave := widget.NewButton("OK", func() {
 		//TODO add server send information
+		//conn, err := net.Dial("tcp", srvAddr)
+		//if err != nil {
+		//	log.Fatalf("Error connect to server: %v\n", err)
+		//}
+		//
+		//_ = conn.Close()
 	})
 
 	btnExit := widget.NewButton("Выход", func() {
@@ -40,5 +66,6 @@ func main() {
 	w.Resize(fyne.NewSize(480, 200))
 	a.Settings().SetTheme(theme.LightTheme())
 
+	// start app
 	w.ShowAndRun()
 }
